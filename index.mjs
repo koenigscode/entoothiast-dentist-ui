aca508671f9395cae3f5d01500a4dd0f5a921b39
Sofia Serbina <serbina@student.chalmers.se>
2023-11-14 18:17:58 +0300

    Add confirmation to cancel appointment #1

--- index.mjs
+++ index.mjs
@@ -4,6 +4,7 @@ import alert from 'js-cli-alerts';
 import Table from 'cli-table';
 import enquirer from 'enquirer';
 const {prompt, Form} = enquirer;
+const { Confirm } = enquirer;
 
 
 //for the progress bar
@@ -11,6 +12,19 @@ const {prompt, Form} = enquirer;
 
 const grayPrompt = '\x1b[90m\x1b[1mPress q to go back\x1b[0m';
 
+function confirmCancellation(){
+  const prompt = new Confirm({
+    name: 'question',
+    message: 'Are you sure you want to cancel this appointment?'
+  });
+  
+  prompt.run()
+    .then(answer => console.log('Answer:', answer))
+    .catch(console.error);
+}
+
+
+//TODO: go back to main menu when q is pressed
 function printGrayPrompt() {
   console.log(grayPrompt);
 }
@@ -41,7 +55,26 @@ const options = [
 // alert({type: `info`, msg: `Awais is awesome!`});
 // alert({type: `error`, msg: `Something went wrong!`});
 console.log("Welcome to Entoothiast!");
-inquirer
+
+function createTimeslot(){
+  //TODO: add check for date and time formats
+  const prompt = new Form({
+    name: 'user',
+    message: 'Please provide the following information to publish a new time slot:',
+    choices: [
+      { name: 'date', message: 'Date', initial: '30/09/2023' },
+      { name: 'time', message: 'Time', initial: '15:30' }
+    ]
+  });
+  
+  prompt.run()
+    .then(value => console.log("New timeslot published"))
+    .catch(console.error);
+}
+
+//TODO: loop main menu
+function mainMenu(){
+  inquirer
   .prompt([
     {
       type: 'list',
@@ -58,23 +91,14 @@ inquirer
         break;
       case "Publish new time slot":
         //alert({type: 'success', msg: "New time slot published!"});
-        const prompt = new Form({
-          name: 'user',
-          message: 'Please provide the following information to publish a new time slot:',
-          choices: [
-            { name: 'date', message: 'Date', initial: '30/09/2023' },
-            { name: 'time', message: 'Time', initial: '15:30' }
-          ]
-        });
-        
-        prompt.run()
-          .then(value => console.log('Answer:', value))
-          .catch(console.error);
-        
+        createTimeslot();
         printGrayPrompt();
         break;
       case "Cancel an appointment":
-        alert({type: 'success', msg: "Appointment cancelled"});
+        let result = confirmCancellation()
+        if (result == true){
+          alert({type: 'success', msg: "Appointment cancelled"});
+        }
         printGrayPrompt();
         break;
       case "Exit":
@@ -82,6 +106,10 @@ inquirer
         break;
     }
   });
+}
+
+mainMenu()
+
 
   
 
